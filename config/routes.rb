53b11aa8296d88580482b456a66b85a31f3a03ca
Damir <damirikq@gmail.com>
2016-10-27 14:00:45 +0300

    devise added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/profile', as: 'user_root'
+
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
