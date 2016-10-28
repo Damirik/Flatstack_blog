bcb70df63619d8f26c1679d3be3499e3fe3dae68
Damir <damirikq@gmail.com>
2016-10-28 19:24:01 +0300

    devise sanitizer updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
 class ApplicationController < ActionController::Base
-  include Authentication
-  include Authorization
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
 
-  responders :flash
-  respond_to :html
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:full_name])
+  end
 end
