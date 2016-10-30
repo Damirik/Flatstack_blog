5149664957e1a8daed417045a969f2de7be02e33
Damir <damirikq@gmail.com>
2016-10-30 22:28:09 +0300

    added blog model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161028103918) do
+ActiveRecord::Schema.define(version: 20161030192536) do
+
+  create_table "blogs", force: :cascade do |t|
+    t.string   "subject"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
