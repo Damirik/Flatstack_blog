dd0731a423c79434ceb8f281eb8c3c3ba5d335f8
Damir <damirikq@gmail.com>
2016-10-31 10:48:58 +0300

    blog validation added+test

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,4 @@
 class Blog < ApplicationRecord
   belongs_to :user
+  validates :subject, presence: true
 end
--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 describe Blog do
   it { belong_to(:user) }
+  it { is_expected.to validate_presence_of :subject }
 end
