ac0587e1077295565917d11ee0f7f37abef3e542
Damir <damirikq@gmail.com>
2016-10-31 22:52:15 +0300

    fixed posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :posts
   root to: "pages#home"
   get 'users/profile', as: 'user_root'
   devise_for :users, controllers: { registrations: "users/registrations" }
