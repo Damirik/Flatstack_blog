78d41fa90e58d917b492cad3b8f502a65fd3f14f
Damir <damirikq@gmail.com>
2016-11-22 12:56:21 +0300

    else blog deleted

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,9 +5,6 @@ module Users
       if @user.persisted?
         flash[:notice] = I18n.t "devise.omniauth_callbacks.success", kind: "Google"
         sign_in_and_redirect @user, event: :authentication
-      else
-        session["devise.google_data"] = request.env["omniauth.auth"]
-        redirect_to new_user_registration_url
       end
     end
   end
