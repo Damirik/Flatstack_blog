4918d70ccf536053d85975592a1569ab58fe2729
Damir <damirikq@gmail.com>
2016-11-22 14:01:45 +0300

    slim-lint passed

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,10 +2,8 @@ module Users
   class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     def google_oauth2
       @user = User.from_omniauth(request.env["omniauth.auth"])
-      if @user.persisted?
-        flash[:notice] = I18n.t "devise.omniauth_callbacks.success", kind: "Google"
-        sign_in_and_redirect @user, event: :authentication
-      end
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", kind: "Google"
+      sign_in_and_redirect @user, event: :authentication
     end
   end
 end
